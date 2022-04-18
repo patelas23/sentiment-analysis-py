591646dbca18917fd21ea5d703428b779d324fbe
Ankur Patel <patelas23@vcu.edu>
2022-04-18 18:07:37 -0400

    pull relevant data from training corpus into lists

--- sentiment.py
+++ sentiment.py
@@ -9,10 +9,16 @@
 #
 # Usage: python3 sentiment.py training_file.txt test_file.txt model_file.txt > output_answers.txt
 #
-# IN: tweets to be analyzed
+# IN: training_file: set of tweets annotated with sentiment classification
+# 	  test_file: set of tweets to be analyzed for sentiment
+#     model_file: name of file to contain sentiment statistics
 # OUT: Trained sentiment classifier
+
 import sys
+import re
+
 def main():
+
     training_file_name = sys.argv[1]
     # test_file_name = sys.argv[2]
     # training_model_file_name = sys.argv[3]
@@ -30,11 +36,23 @@ def train_model():
 def apply_model():
     pass
 
-def clean_text():
+def clean_text(corpus_text):
+    re.sub(r'', "", corpus_text)
+
+def count_features(feature, context_line):
     pass
 
-def count_features():
+# Helper function returns a list of lines from corpus
+#     demarcated by:
+#         <context> This is a tweet. </context>
+def extract_context(corpus_text):
     pass
+
+# Helper function returns a list of sentiment tags
+#     corresponding to lines of context
+def extract_sentiment(corpus_text):
+    return re.findall(r'senseid="(\S+)/>"', corpus_text)
+
 
 if __name__ == "__main__":
     main()
