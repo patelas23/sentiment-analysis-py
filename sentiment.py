5a5cc7a5ae41492ce3e9401fbf936f79685eeebb
Ankur Patel <patelas23@vcu.edu>
2022-04-18 19:54:15 -0400

    extract and clean text data from training file

--- sentiment.py
+++ sentiment.py
@@ -1,4 +1,4 @@
-# sentiment.py 
+# sentiment.py
 # Author: Ankur Patel
 # Course: Natural Language Processing
 # CourseID: CMSC416-SP2022
@@ -7,10 +7,11 @@
 # A decision-list based sentiment classifier for
 # identifying sentiment from a given sample of tweets
 #
-# Usage: python3 sentiment.py training_file.txt test_file.txt model_file.txt > output_answers.txt
+# Usage: python3 sentiment.py training_file.txt test_file.txt model_file.txt
+#         > output_answers.txt
 #
 # IN: training_file: set of tweets annotated with sentiment classification
-# 	  test_file: set of tweets to be analyzed for sentiment
+#       test_file: set of tweets to be analyzed for sentiment
 #     model_file: name of file to contain sentiment statistics
 # OUT: Trained sentiment classifier
 
@@ -27,17 +28,17 @@ def main():
     training_text = ""
     test_text = ""
     model_text = ""
-	with open(training_file_name) as f:
-    	training_text = f.read()
+    with open(training_file_name) as f:
+        training_text = f.read()
 
-def train_model():
+    print(extract_context(training_text))
+
+def train_model(context_lines, sentiment_lines):
     pass
 
 def apply_model():
     pass
 
-def clean_text(corpus_text):
-    re.sub(r'', "", corpus_text)
 
 def count_features(feature, context_line):
     pass
@@ -46,13 +47,16 @@ def count_features(feature, context_line):
 #     demarcated by:
 #         <context> This is a tweet. </context>
 def extract_context(corpus_text):
-    pass
+    print("here")
+    return re.findall(r'<context>\s+(.*)\s+</context>', corpus_text)
 
 # Helper function returns a list of sentiment tags
 #     corresponding to lines of context
 def extract_sentiment(corpus_text):
-    return re.findall(r'senseid="(\S+)/>"', corpus_text)
+    return re.findall(r'sentiment="(\S+)"/>', corpus_text)
 
+def clean_text(corpus_text):
+    re.sub(r'', "", corpus_text)
 
 if __name__ == "__main__":
     main()
